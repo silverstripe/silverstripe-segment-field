c1568e1d8cb9c25ea92812bc7dc6af66c6581aac
Robbie Averill <robbie@silverstripe.com>
2017-02-02 11:15:38 +1300

    FIX Update JS for new CMS classes, remove jQuery mobile use, add Bootstrap classes and minor CSS

--- public/segment-field.dist.js
+++ public/segment-field.dist.js
@@ -2,137 +2,137 @@
 'use strict';
 
 (function ($) {
-	$.entwine('ss', function ($) {
-		$('.field.silverstripe\\\\forms\\\\segment:not(.readonly)').entwine({
-			'MaxPreviewLength': 55,
+  $.entwine('ss', function ($) {
+    $('.cms .field.segment:not(.readonly)').entwine({
+      'MaxPreviewLength': 55,
 
-			'Ellipsis': '...',
+      'Ellipsis': '...',
 
-			'onmatch': function onmatch() {
-				if (this.find(':text').length) {
-					this.toggleEdit(false);
-				}
+      'onmatch': function onmatch() {
+        if (this.find(':text').length) {
+          this.toggleEdit(false);
+        }
 
-				this.redraw();
-				this._super();
-			},
+        this.redraw();
+        this._super();
+      },
 
-			'redraw': function redraw() {
-				var field = this.find(':text');
-				var value = field.val();
+      'redraw': function redraw() {
+        var field = this.find(':text');
+        var value = field.val();
 
-				var preview = value;
+        var preview = value;
 
-				if (value.length > this.getMaxPreviewLength()) {
-					preview = this.getEllipsis() + value.substr(value.length - this.getMaxPreviewLength(), value.length);
-				}
+        if (value.length > this.getMaxPreviewLength()) {
+          preview = this.getEllipsis() + value.substr(value.length - this.getMaxPreviewLength(), value.length);
+        }
 
-				this.find('.preview').text(field.data('preview'));
-			},
+        this.find('.preview').text(field.data('preview'));
+      },
 
-			'toggleEdit': function toggleEdit(toggle) {
-				var field = this.find(':text');
+      'toggleEdit': function toggleEdit(toggle) {
+        var field = this.find(':text');
 
-				this.find('.preview-holder')[toggle ? 'hide' : 'show']();
-				this.find('.edit-holder')[toggle ? 'show' : 'hide']();
+        this.find('.preview-holder')[toggle ? 'hide' : 'show']();
+        this.find('.edit-holder')[toggle ? 'show' : 'hide']();
 
-				if (toggle) {
-					field.data('original', field.val());
-					field.focus();
-				}
-			},
+        if (toggle) {
+          field.data('original', field.val());
+          field.focus();
+        }
+      },
 
-			'update': function update() {
-				var _this = this;
+      'update': function update() {
+        var _this = this;
 
-				var field = this.find(':text');
-				var current = field.data('original');
-				var updated = field.val();
+        var field = this.find(':text');
+        var current = field.data('original');
+        var updated = field.val();
 
-				if (current != updated) {
-					this.addClass('loading');
+        if (current != updated) {
+          this.addClass('loading');
 
-					this.suggest(updated, function (data) {
-						_this.removeClass('loading');
+          this.suggest(updated, function (data) {
+            _this.removeClass('loading');
 
-						field.val(data['suggestion']);
-						field.data('preview', data['preview']);
+            field.val(data['suggestion']);
+            field.data('preview', data['preview']);
 
-						_this.toggleEdit(false);
-						_this.redraw();
-					});
-				} else {
-					this.toggleEdit(false);
-					this.redraw();
-				}
-			},
+            _this.toggleEdit(false);
+            _this.redraw();
+          });
+        } else {
+          this.toggleEdit(false);
+          this.redraw();
+        }
+      },
 
-			'cancel': function cancel() {
-				var field = this.find(':text');
-				field.val(field.data("original"));
-				this.toggleEdit(false);
-			},
+      'cancel': function cancel() {
+        var field = this.find(':text');
+        field.val(field.data("original"));
+        this.toggleEdit(false);
+      },
 
-			'suggest': function suggest(val, callback) {
-				var _this2 = this;
+      'suggest': function suggest(val, callback) {
+        var _this2 = this;
 
-				var field = this.find(':text');
-				var parts = $.path.parseUrl(this.closest('form').attr('action'));
+        var field = this.find(':text');
+        var parts = this.closest('form').attr('action').split('?');
 
-				var url = parts.hrefNoSearch + '/field/' + field.attr('name') + '/suggest/?value=' + encodeURIComponent(val);
+        var url = parts[0] + '/field/' + field.attr('name') + '/suggest/?value=' + encodeURIComponent(val);
 
-				if (parts.search) {
-					url += '&' + parts.search.replace(/^\?/, '');
-				}
+        if (parts[1]) {
+          url += '&' + parts[1].replace(/^\?/, '');
+        }
 
-				$.ajax({
-					'url': url,
-					'dataType': 'json',
-					'success': function success(data) {
-						callback(data);
-					},
-					'error': function error(xhr, status) {
-						xhr.statusText = xhr.responseText;
-					},
-					'complete': function complete() {
-						_this2.removeClass('loading');
-					}
-				});
-			}
-		});
+        $.ajax({
+          'url': url,
+          'dataType': 'json',
+          'success': function success(data) {
+            callback(data);
+          },
+          'error': function error(xhr, status) {
+            xhr.statusText = xhr.responseText;
+          },
+          'complete': function complete() {
+            _this2.removeClass('loading');
+          }
+        });
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .edit').entwine({
-			'onclick': function onclick(event) {
-				event.preventDefault();
-				this.closest('.field').toggleEdit(true);
-			}
-		});
+    $('.cms .field.segment .edit').entwine({
+      'onclick': function onclick(event) {
+        event.preventDefault();
+        this.closest('.field').toggleEdit(true);
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .update').entwine({
-			'onclick': function onclick(event) {
-				event.preventDefault();
-				this.closest('.field').update();
-			}
-		});
+    $('.cms .field.segment .update').entwine({
+      'onclick': function onclick(event) {
+        event.preventDefault();
+        this.closest('.field').update();
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .cancel').entwine({
-			'onclick': function onclick(event) {
-				event.preventDefault();
-				this.closest('.field').cancel();
-			}
-		});
+    $('.cms .field.segment .cancel').entwine({
+      'onclick': function onclick(event) {
+        event.preventDefault();
+        this.closest('.field').cancel();
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment :text').entwine({
-			'onkeydown': function onkeydown(event) {
-				var code = event.keyCode || event.which;
+    $('.cms .field.segment :text').entwine({
+      'onkeydown': function onkeydown(event) {
+        var code = event.keyCode || event.which;
 
-				if (code == 13) {
-					event.stop();
-					this.closest('.field').update();
-				}
-			}
-		});
-	});
+        if (code == 13) {
+          event.stop();
+          this.closest('.field').update();
+        }
+      }
+    });
+  });
 })(jQuery);
 
 },{}]},{},[1]);
--- public/segment-field.js
+++ public/segment-field.js
@@ -1,130 +1,130 @@
 (function ($) {
-	$.entwine('ss', function ($) {
-		$('.field.silverstripe\\\\forms\\\\segment:not(.readonly)').entwine({
-			'MaxPreviewLength': 55,
+  $.entwine('ss', function ($) {
+    $('.cms .field.segment:not(.readonly)').entwine({
+      'MaxPreviewLength': 55,
 
-			'Ellipsis': '...',
+      'Ellipsis': '...',
 
-			'onmatch': function () {
-				if (this.find(':text').length) {
-					this.toggleEdit(false);
-				}
+      'onmatch': function () {
+        if (this.find(':text').length) {
+          this.toggleEdit(false);
+        }
 
-				this.redraw();
-				this._super();
-			},
+        this.redraw();
+        this._super();
+      },
 
-			'redraw': function () {
-				var field = this.find(':text');
-				var value = field.val();
+      'redraw': function () {
+        var field = this.find(':text');
+        var value = field.val();
 
-				var preview = value;
+        var preview = value;
 
-				if (value.length > this.getMaxPreviewLength()) {
-					preview = this.getEllipsis() + value.substr(value.length - this.getMaxPreviewLength(), value.length);
-				}
+        if (value.length > this.getMaxPreviewLength()) {
+          preview = this.getEllipsis() + value.substr(value.length - this.getMaxPreviewLength(), value.length);
+        }
 
-				this.find('.preview').text(field.data('preview'));
-			},
+        this.find('.preview').text(field.data('preview'));
+      },
 
-			'toggleEdit': function (toggle) {
-				var field = this.find(':text');
+      'toggleEdit': function (toggle) {
+        var field = this.find(':text');
 
-				this.find('.preview-holder')[toggle ? 'hide' : 'show']();
-				this.find('.edit-holder')[toggle ? 'show' : 'hide']();
+        this.find('.preview-holder')[toggle ? 'hide' : 'show']();
+        this.find('.edit-holder')[toggle ? 'show' : 'hide']();
 
-				if (toggle) {
-					field.data('original', field.val());
-					field.focus();
-				}
-			},
+        if (toggle) {
+          field.data('original', field.val());
+          field.focus();
+        }
+      },
 
-			'update': function () {
-				var field = this.find(':text');
-				var current = field.data('original');
-				var updated = field.val();
+      'update': function () {
+        var field = this.find(':text');
+        var current = field.data('original');
+        var updated = field.val();
 
-				if (current != updated) {
-					this.addClass('loading');
+        if (current != updated) {
+          this.addClass('loading');
 
-					this.suggest(updated, (data) => {
-						this.removeClass('loading');
+          this.suggest(updated, (data) => {
+            this.removeClass('loading');
 
-						field.val(data['suggestion']);
-						field.data('preview', data['preview']);
+            field.val(data['suggestion']);
+            field.data('preview', data['preview']);
 
-						this.toggleEdit(false);
-						this.redraw();
-					});
-				} else {
-					this.toggleEdit(false);
-					this.redraw();
-				}
-			},
+            this.toggleEdit(false);
+            this.redraw();
+          });
+        } else {
+          this.toggleEdit(false);
+          this.redraw();
+        }
+      },
 
-			'cancel': function () {
-				var field = this.find(':text');
-				field.val(field.data("original"));
-				this.toggleEdit(false);
-			},
+      'cancel': function () {
+        var field = this.find(':text');
+        field.val(field.data("original"));
+        this.toggleEdit(false);
+      },
 
-			'suggest': function (val, callback) {
-				var field = this.find(':text');
-				var parts = $.path.parseUrl(this.closest('form').attr('action'));
+      'suggest': function (val, callback) {
+        var field = this.find(':text');
+        var parts = this.closest('form').attr('action').split('?');
 
-				var url = parts.hrefNoSearch + '/field/' + field.attr('name') + '/suggest/?value=' + encodeURIComponent(val);
+        var url = parts[0] + '/field/' + field.attr('name') + '/suggest/?value=' + encodeURIComponent(val);
 
-				if (parts.search) {
-					url += '&' + parts.search.replace(/^\?/, '');
-				}
+        if (parts[1]) {
+          url += '&' + parts[1].replace(/^\?/, '');
+        }
 
-				$.ajax({
-					'url': url,
-					'dataType': 'json',
-					'success': (data) => {
-						callback(data);
-					},
-					'error': (xhr, status) => {
-						xhr.statusText = xhr.responseText;
-					},
-					'complete': () => {
-						this.removeClass('loading');
-					}
-				});
-			}
-		});
+        $.ajax({
+          'url': url,
+          'dataType': 'json',
+          'success': (data) => {
+            callback(data);
+          },
+          'error': (xhr, status) => {
+            xhr.statusText = xhr.responseText;
+          },
+          'complete': () => {
+            this.removeClass('loading');
+          }
+        });
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .edit').entwine({
-			'onclick': function (event) {
-				event.preventDefault();
-				this.closest('.field').toggleEdit(true);
-			}
-		});
+    $('.cms .field.segment .edit').entwine({
+      'onclick': function (event) {
+        event.preventDefault();
+        this.closest('.field').toggleEdit(true);
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .update').entwine({
-			'onclick': function (event) {
-				event.preventDefault();
-				this.closest('.field').update();
-			}
-		});
+    $('.cms .field.segment .update').entwine({
+      'onclick': function (event) {
+        event.preventDefault();
+        this.closest('.field').update();
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment .cancel').entwine({
-			'onclick': function (event) {
-				event.preventDefault();
-				this.closest('.field').cancel();
-			}
-		});
+    $('.cms .field.segment .cancel').entwine({
+      'onclick': function (event) {
+        event.preventDefault();
+        this.closest('.field').cancel();
+      }
+    });
 
-		$('.field.silverstripe\\\\forms\\\\segment :text').entwine({
-			'onkeydown': function (event) {
-				var code = event.keyCode || event.which;
+    $('.cms .field.segment :text').entwine({
+      'onkeydown': function (event) {
+        var code = event.keyCode || event.which;
 
-				if(code == 13) {
-					event.stop();
-					this.closest('.field').update();
-				}
-			}
-		});
-	});
+        if(code == 13) {
+          event.stop();
+          this.closest('.field').update();
+        }
+      }
+    });
+  });
 
 }(jQuery));
